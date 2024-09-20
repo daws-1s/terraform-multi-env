cf96f9fa57fbf538f20ddeadc8874d39871559fa
divya kamatham <divyakamatham12@gmai.com>
2024-09-20 23:20:42 +0800

    Add files

---
+++ workspaces/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "1s-remote-state"
+    key    = "workspaces"
+    region = "us-east-1"
+    dynamodb_table = "1s-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
